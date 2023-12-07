1641a6cf0070957f2099cea85e147aeba068f409
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-12-07 05:17:17 +0100

    Attempting render and render fix

--- file.py
+++ file.py
@@ -1,3 +1,15 @@
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import RIGHT_ONLY
 from nes_py.wrappers import JoypadSpace
+
+ENV_NAME = 'SuperMarioBros-1-1-v0'
+env = gym_super_mario_bros.make(ENV_NAME, render_mode='human' if DISPLAY else 'rgb', apply_api_compatibility=True)
+env = JoypadSpace(env, RIGHT_ONLY)
+
+done = False
+env.reset()
+
+while not done:
+    action = RIGHT_ONLY.index(['right'])
+    _, _, done, _, _ = env.step(action)
+    env.render()
