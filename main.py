257bbad461ead4afce66164902159c7c78d8dab9
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-12-07 06:05:16 +0100

    Main init

---
+++ main.py
@@ -0,0 +1,6 @@
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import RIGHT_ONLY
+from nes_py.wrappers import JoypadSpace
+
+from wrappers import apply_wrappers
+from agent import Agent
